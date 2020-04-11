f58f380134eba1e7b94ef193b2cf98ed885c07d5
skalutki <skalutki@gmail.com>
2020-04-11 09:07:27 +0100

    feat: update npm, add js to project

--- src/js/main.js
+++ src/js/main.js
@@ -3,20 +3,37 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
     });
-  });
 }
 
 // place your code below
 
+const input = document.querySelector('.editor__story__input--js');
+const load = document.querySelector('.editor__button__load--js');
+const save = document.querySelector('.editor__button__save--js');
+const trash = document.querySelector('.editor__button__trash--js');
 
-console.log(`Hello world!`);
 
 
+save.addEventListener('click', (e) => {
+    e.preventDefault();
+    localStorage.setItem('input', input.value);
+});
+
+load.addEventListener('click', (e) => {
+    e.preventDefault();
+    input.value = localStorage.getItem('input');
+});
+
+trash.addEventListener('click', (e) => {
+    e.preventDefault();
+    input.value = localStorage.removeItem('input');
+});
